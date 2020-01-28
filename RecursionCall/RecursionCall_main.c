863992e53059be18e6e8ebb220246c26fd5540a9
pikacsc <ccc11203@gmail.com>
2020-01-28 15:47:56 +0900

    recursive call

---
+++ RecursionCall/RecursionCall_main.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void my_func(int);
+
+int main(void)
+{
+	my_func(1);
+
+	return 0;
+}
+
+void my_func(int n)
+{
+	printf("Level %d, address of variable n = %p\n", n, &n);
+
+	//TODO: stop condition
+	if(n <= 4)
+		my_func(n + 1);
+
+	
+}
