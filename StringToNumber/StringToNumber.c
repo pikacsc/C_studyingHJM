ec880dd4a06a58cbd616d662c462520c3b4d13d3
pikacsc <ccc11203@gmail.com>
2020-01-28 16:45:57 +0900

    atoi, atof, atol

---
+++ StringToNumber/StringToNumber.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	/*
+		string to integer, double, long
+		atoi(), atof(), atol()
+	*/
+
+	//if (argc < 3)
+	//	printf("Wrong Usage of %s\n", argv[0]);
+	//else
+	//{
+	//	/* Example 1 */
+
+	//	int times = atoi(argv[1]);
+	//	// atof(), atol()
+
+	//	for (int i = 0; i < times; i++)
+	//		puts(argv[2]);
+
+	//	/* Example 2 */
+
+	//	printf("Sum = %d\n", atoi(argv[1]) + atoi(argv[2]));
+	//}
+
+
+	return 0;
+}
