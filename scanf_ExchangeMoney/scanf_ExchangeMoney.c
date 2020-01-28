333d7794e7d619aaa571fc75c3e143e94b695fb0
pikacsc <ccc11203@gmail.com>
2020-01-28 14:55:19 +0900

    scanf_exchangeMoney

---
+++ scanf_ExchangeMoney/scanf_ExchangeMoney.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS //only for visual studio
+#include <stdio.h>
+
+int main()
+{
+	int KRW = 0;
+	float USD = 0;
+	float exchangeRateUSD = 0.00089f;
+	printf("input KRW : ");
+	scanf("%d",&KRW);
+	USD = KRW * exchangeRateUSD;
+
+	printf("KRW : %d \n", KRW);
+	printf("USD : %f \n", USD);
+
+}
