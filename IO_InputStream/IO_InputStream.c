ac63adf7f3510487720083195f1f406a7d359386
pikacsc <ccc11203@gmail.com>
2020-01-28 15:46:43 +0900

    IO input stream

---
+++ IO_InputStream/IO_InputStream.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char str[255];
+	int i, i2;
+	double d;
+
+	scanf("%s ,%d lf", str, &i, &d);
+	printf("%s %d %f\n", str, i, d);
+
+	// or (see the difference)
+
+	scanf("%s %d %d", str, &i, &i2);
+	printf("%s %d %d\n", str, i, i2);
+
+	// or (see the differenc)
+
+	char c;
+	while ((c = getchar()) != '\n')
+		putchar(c);
+	printf("\n");
+}
